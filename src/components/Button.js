4bbbfb6cf266f041909cec772a73691102d15b31
shuhaibp7 <pshuhaib077@gmail.com>
2021-02-24 03:32:17 -0800

    Register

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+function Button () {
+    return (
+        <View >
+            <Text>Hi</Text>
+        </View>
+    )
+}
+
+export default Button;
